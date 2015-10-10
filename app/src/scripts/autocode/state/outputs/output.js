863cccc4897b9b67d75b63449276e3cecb147c6f
Chris Tate <chris@studiotate.com>
2015-10-10 18:09:32 -0500

    json schema form

--- app/src/scripts/autocode/state/outputs/output.js
+++ app/src/scripts/autocode/state/outputs/output.js
@@ -7,4 +7,28 @@ autocode.state['outputs/output'] = function(opts) {
   $('#outputs-content input[name="generator"]').val(outputed.generator);
   
   $('#outputs-content textarea').val(jsyaml.safeDump(outputed.spec));
+  
+  $('#outputs-content .content-center .schema').empty();
+  
+  var schema = generator.schema;
+  
+  if (!schema || !schema.properties) {
+    return;
+  }
+  
+  var property, property_title;
+  for (var property_name in schema.properties) {
+    property = schema.properties[property_name];
+    
+    if (property.title) {
+      property_title = property.title;
+    } else {
+      property_title = property_name;
+    }
+    
+    $('#outputs-content .content-center .schema').append(
+      '<label>' + property_title + ' <img height="16" src="images/info.svg" style="vertical-align: middle" title="' + property.description + '" /></label>'
+      + '<input spellcheck="false" type="text" value="' + outputed.spec[property_name] + '" />'
+    );
+  }
 };
