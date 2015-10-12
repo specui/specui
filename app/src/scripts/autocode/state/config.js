b9a41aa65a244cd8025720c52508bf0610215506
Chris Tate <chris@studiotate.com>
2015-10-12 01:49:21 -0500

    fixed code resize

--- app/src/scripts/autocode/state/config.js
+++ app/src/scripts/autocode/state/config.js
@@ -1,12 +1,21 @@
 autocode.state['config'] = function() {
+  var code_mirror = $('#config-content .CodeMirror');
+  var value = jsyaml.safeDump(autocode.project);
+  
   autocode.action.toggleSection('config');
   
-  if (!$('#config-content .CodeMirror').length) {
+  if (!code_mirror.length) {
     var editor = CodeMirror.fromTextArea($('#config-content textarea')[0], {
       lineNumbers: true,
       mode: 'yaml'
     });
+    
+    code_mirror = $('#config-content .CodeMirror')
+    code_mirror[0].CodeMirror.setValue(value);
+    
+    $('.CodeMirror-scroll').scrollTop(2);
+    
+  } else {
+    code_mirror[0].CodeMirror.setValue(value);
   }
-  
-  $('#config-content .CodeMirror')[0].CodeMirror.setValue(jsyaml.safeDump(autocode.project));
 };
