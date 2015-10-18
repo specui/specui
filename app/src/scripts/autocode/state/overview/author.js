182653cf494651992e38a8a2940197b68bb8647c
Chris Tate <chris@studiotate.com>
2015-10-18 14:12:54 -0500

    fixed missing info

--- app/src/scripts/autocode/state/overview/author.js
+++ app/src/scripts/autocode/state/overview/author.js
@@ -1,9 +1,9 @@
 autocode.state['overview/author'] = function() {
   autocode.action.toggleColumn('overview-content', 2);
   
-  $('#overview-author-name .value').text(autocode.project.author.name);
-  $('#overview-author-email .value').text(autocode.project.author.email);
-  $('#overview-author-url .value').text(autocode.project.author.url);
+  $('#overview-author-name .value').text(autocode.project.author.name || ' [ Click to Add ]');
+  $('#overview-author-email .value').text(autocode.project.author.email || ' [ Click to Add ]');
+  $('#overview-author-url .value').text(autocode.project.author.url || ' [ Click to Add ]');
   
   autocode.action.toggleSection('overview', 'author');
 };
--- app/src/scripts/autocode/state/overview/general.js
+++ app/src/scripts/autocode/state/overview/general.js
@@ -9,5 +9,5 @@ autocode.state['overview/general'] = function() {
     ? '<a href="' + autocode.project.url + '">' + autocode.project.url + '</a>'
     : '[ Click to Add ]'
   );
-  $('#overview-general-copyright .value').text(autocode.project.copyright);
+  $('#overview-general-copyright .value').text(autocode.project.copyright || ' [ Click to Add ]');
 };
