f51bcf1b0c20a1498501bde6c39aef7eca8b3530
Chris Tate <chris@ctate.dev>
2023-11-07 22:10:05 -0600

    adds more monaco workers

--- apps/web/components/ZappEditor/ZappEditor.tsx
+++ apps/web/components/ZappEditor/ZappEditor.tsx
@@ -206,6 +206,27 @@ export const ZappEditor: FC = () => {
             return new Worker(
               new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url),
             );
+          case 'css':
+          case 'less':
+          case 'scss':
+            return new Worker(
+              new URL('monaco-editor/esm/vs/language/css/css.worker', import.meta.url),
+            );
+          case 'handlebars':
+          case 'html':
+          case 'razor':
+            return new Worker(
+              new URL('monaco-editor/esm/vs/language/html/html.worker', import.meta.url),
+            );
+          case 'json':
+            return new Worker(
+              new URL('monaco-editor/esm/vs/language/json/json.worker', import.meta.url),
+            );
+          case 'javascript':
+          case 'typescript':
+            return new Worker(
+              new URL('monaco-editor/esm/vs/language/typescript/ts.worker', import.meta.url),
+            );
           case 'yaml':
             return new Worker(new URL('monaco-yaml/yaml.worker', import.meta.url));
           default:
