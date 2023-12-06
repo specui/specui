22539f73e4ed1ccffec6b17a95806cf4672be8d6
Chris Tate <chris@ctate.dev>
2023-12-05 20:43:43 -0600

    adds 404 page

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <main
+      className="flex flex-col gap-2 items-center justify-center mx-auto"
+      style={{ minHeight: 'calc(100vh - 65px)' }}
+    >
+      <h2 className="text-4xl">Not Found</h2>
+      <p className="text-gray-400">
+        Go back{' '}
+        <Link className="text-yellow-300" href="/">
+          home
+        </Link>
+      </p>
+    </main>
+  );
+}
