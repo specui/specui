a2964903d4dc0f1cda057c4dd53a9b82944aee91
Chris Tate <chris@ctate.dev>
2024-01-09 02:01:23 -0600

    better height detection in code snippets

--- apps/web/components/CodeSnippet.tsx
+++ apps/web/components/CodeSnippet.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 
 interface Props {
   children?: string;
@@ -7,16 +7,39 @@ interface Props {
 }
 
 export const CodeSnippet: FC<Props> = ({ children, className, title }) => {
+  const titleRef = useRef<HTMLDivElement>(null);
+
+  const [height, setHeight] = useState(48);
+
+  const handleResize = () => {
+    setHeight(titleRef.current ? titleRef.current.clientHeight : 48);
+  };
+
+  useEffect(() => {
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className={`border border-gray-800 overflow-auto rounded-md text-white ${className}`}>
-      <div className="border-b border-gray-900 flex gap-2 items-center justify-center p-3">
+    <div className={`border border-gray-800 rounded-md text-white ${className}`}>
+      <div
+        className="border-b border-gray-900 flex gap-2 items-center justify-center p-3"
+        ref={titleRef}
+      >
         <div className="border border-gray-600 rounded-full w-3 h-3" />
         <div className="border border-gray-600 rounded-full w-3 h-3" />
         <div className="border border-gray-600 rounded-full w-3 h-3" />
         <div className="flex-grow text-center text-gray-500">{title}</div>
         <div className="w-12" />
       </div>
-      <pre className="p-3 text-xs w-full h-full">{children?.trim()}</pre>
+      <pre className={`overflow-auto p-3 text-xs w-full h-[calc(100%-${height}px)]`}>
+        {children?.trim()}
+      </pre>
     </div>
   );
 };
