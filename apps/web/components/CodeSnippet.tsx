4d90811a1b6c804fb442e409fc96be27a55c6aaa
Chris Tate <chris@ctate.dev>
2024-01-09 01:50:55 -0600

    fixes scrollbars

--- apps/web/components/CodeSnippet.tsx
+++ apps/web/components/CodeSnippet.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const CodeSnippet: FC<Props> = ({ children, className, title }) => {
   return (
-    <div className={`border border-gray-800 rounded-md text-white ${className}`}>
+    <div className={`border border-gray-800 overflow-auto rounded-md text-white ${className}`}>
       <div className="border-b border-gray-900 flex gap-2 items-center justify-center p-3">
         <div className="border border-gray-600 rounded-full w-3 h-3" />
         <div className="border border-gray-600 rounded-full w-3 h-3" />
@@ -16,7 +16,7 @@ export const CodeSnippet: FC<Props> = ({ children, className, title }) => {
         <div className="flex-grow text-center text-gray-500">{title}</div>
         <div className="w-12" />
       </div>
-      <pre className="overflow-auto p-3 text-xs w-full h-full">{children?.trim()}</pre>
+      <pre className="p-3 text-xs w-full h-full">{children?.trim()}</pre>
     </div>
   );
 };
