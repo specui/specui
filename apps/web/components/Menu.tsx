25b381dca6ae89db15d9861fa85bf0b2784423ba
Chris Tate <chris@ctate.dev>
2024-08-05 06:55:22 -0500

    fixes menu

--- apps/web/components/Menu.tsx
+++ apps/web/components/Menu.tsx
@@ -4,7 +4,8 @@ import { MenuRounded } from '@mui/icons-material';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { FC, useState } from 'react';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 
 const pages = [
   {
@@ -17,9 +18,7 @@ const pages = [
   },
 ];
 
-export const Menu: FC = () => {
-  const pathname = usePathname();
-
+export function Menu() {
   const [show, setShow] = useState(false);
 
   return (
@@ -27,26 +26,45 @@ export const Menu: FC = () => {
       <button className="md:hidden" onClick={() => setShow(!show)}>
         <MenuRounded className="text-4xl xs:text-3xl sm:text-2xl" />
       </button>
-      <ul
-        className={clsx(
-          'bg-slate-900 bottom-0 gap-8 items-center justify-center fixed left-0 right-0 text-6xl top-0 z-10 md:bg-transparent md:flex md:static md:text-base',
-          show ? 'flex flex-col md:flex-row' : 'hidden',
-        )}
-      >
-        {pages.map((page) => (
-          <li
-            className={clsx(
-              'text-center',
-              pathname.startsWith(page.url) ? 'text-blue-600' : 'text-gray-200 hover:text-white',
-            )}
-            key={page.url}
-          >
-            <Link href={page.url} onMouseUp={() => setShow(false)}>
-              {page.text}
-            </Link>
-          </li>
-        ))}
+      <ul className="hidden md:flex">
+        <MenuContent />
       </ul>
+      <MobileMenu onSelect={() => setShow(!show)} show={show} />
     </>
   );
-};
+}
+
+function MenuContent({ onSelect }: { onSelect?: () => void }) {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {pages.map((page) => (
+        <li
+          className={clsx(
+            'text-center',
+            pathname.startsWith(page.url) ? 'text-blue-600' : 'text-gray-200 hover:text-white',
+          )}
+          key={page.url}
+        >
+          <Link href={page.url} onClick={onSelect}>
+            {page.text}
+          </Link>
+        </li>
+      ))}
+    </>
+  );
+}
+
+function MobileMenu({ onSelect, show }: { onSelect?: () => void; show: boolean }) {
+  if (!show) {
+    return null;
+  }
+
+  return createPortal(
+    <ul className="backdrop-blur-2xl bg-slate-900 bg-opacity-50 bottom-0 flex flex-col fixed gap-4 justify-center items-center left-0 right-0 text-4xl top-0 md:hidden z-10">
+      <MenuContent onSelect={onSelect} />
+    </ul>,
+    document.body,
+  );
+}
