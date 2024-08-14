021675086c064e5c6881d56ec63ff11fa7cfcb34
Chris Tate <chris@ctate.dev>
2024-08-14 09:10:29 -0500

    Revert "updates next example"
    This reverts commit cca2e6a60b2b14cb402efc30dee0e68d6512ce22.

--- examples/next-api/app/layout.tsx
+++ examples/next-api/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -13,10 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
