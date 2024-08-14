d4cf4d332906924da9088bf4a4a2e82ce81fec67
Chris Tate <chris@ctate.dev>
2024-08-14 07:48:05 -0500

    simplifies next spec

--- generators/next/.specui/interfaces/ISpec.ts
+++ generators/next/.specui/interfaces/ISpec.ts
@@ -1,88 +1,133 @@
-import type { BaseElement } from './BaseElement';
-import type {
-  AccordionComponent,
-  AccordionItemComponent,
-  AccordionTriggerComponent,
-  AccordionContentComponent,
-  AlertComponent,
-  AlertTitleComponent,
-  AlertDescriptionComponent,
-  AlertDialogComponent,
-  AlertDialogContentComponent,
-  AlertDialogOverlayComponent,
-  AlertDialogPortalComponent,
-  AlertDialogTriggerComponent,
-  AspectRatioComponent,
-  AvatarComponent,
-  AvatarFallbackComponent,
-  AvatarImageComponent,
-  BadgeComponent,
-  BreadcrumbComponent,
-  BreadcrumbEllipsisComponent,
-  BreadcrumbItemComponent,
-  BreadcrumbLinkComponent,
-  BreadcrumbListComponent,
-  BreadcrumbPageComponent,
-  BreadcrumbSeparatorComponent,
-  ButtonComponent,
-  CalendarComponent,
-  CardComponent,
-  CardContentComponent,
-  CardDescriptionComponent,
-  CardFooterComponent,
-  CardHeaderComponent,
-  CardTitleComponent,
-  CarouselComponent,
-  CarouselContentComponent,
-  CarouselItemComponent,
-  CarouselNextComponent,
-  CarouselPreviousComponent,
-  CheckboxComponent,
-  CheckboxIndicatorComponent,
-  CollapsibleComponent,
-  CollapsibleContentComponent,
-  CollapsibleTriggerComponent,
-  CommandComponent,
-  CommandEmptyComponent,
-  CommandGroupComponent,
-  CommandInputComponent,
-  CommandItemComponent,
-  CommandListComponent,
-  CommandSeparatorComponent,
-  CommandShortcutComponent,
-  ContextMenuCheckboxItemComponent,
-  ContextMenuComponent,
-  ContextMenuContentComponent,
-  ContextMenuItemComponent,
-  ContextMenuLabelComponent,
-  ContextMenuRadioGroupComponent,
-  ContextMenuRadioItemComponent,
-  ContextMenuSeparatorComponent,
-  ContextMenuSubComponent,
-  ContextMenuSubContentComponent,
-  ContextMenuSubTriggerComponent,
-  ContextMenuTriggerComponent,
-} from './shadcn';
+type BlendMode =
+  | 'normal'
+  | 'multiply'
+  | 'screen'
+  | 'overlay'
+  | 'darken'
+  | 'lighten'
+  | 'color-dodge'
+  | 'color-burn'
+  | 'hard-light'
+  | 'soft-light'
+  | 'difference'
+  | 'exclusion'
+  | 'hue'
+  | 'saturation'
+  | 'color'
+  | 'luminosity';
 
-type Provider = 'facebook' | 'github' | 'google';
+export interface Style {
+  background?: string;
+  backgroundAttachment?: 'fixed' | 'local' | 'scroll';
+  backgroundBlendMod?: BlendMode;
+  backgroundClip?: 'border-box' | 'padding-box' | 'content-box' | 'text';
+  backgroundColor?: string;
+  backgroundImage?: string;
+  color?: string;
+  fontFamily?: string;
+}
 
-export interface Page {
-  dataSources?: Record<
-    string,
-    {
-      type: 'model';
-      model?: string;
-    }
-  >;
-  elements?: ElementArrayOrRef;
+interface Author {
+  name?: string;
+  email?: string;
+  url?: string;
+}
+
+type License = 'Apache-2.0' | 'GPL-2.0-only' | 'GPL-3.0-only' | 'ISC' | 'MIT';
+type PrimitiveType = 'boolean' | 'number' | 'string';
+
+interface Component {
+  props?: Record<string, ComponentProp>;
+  elements?: Element[];
+}
+
+interface ComponentProp {
+  required?: boolean;
+  type?: PrimitiveType;
+}
+
+interface Page {
+  title?: string;
+  elements?: Element[];
 }
 
 export type Element =
-  | BaseElement
   | AElement
+  | DivElement
+  | HeadingElement
   | IframeElement
   | ImgElement
   | InputElement
+  | LiElement
+  | SectionElement
+  | SpanElement
+  | UlElement
+  | ShadcnComponent;
+
+export interface BaseElement {
+  class?: string | string[];
+  elements?: Element[];
+  style?: Style;
+  text?: string;
+}
+
+interface AElement extends BaseElement {
+  tag: 'a';
+  href?: string;
+}
+
+interface DivElement extends BaseElement {
+  tag: 'div';
+}
+
+interface HeadingElement extends BaseElement {
+  tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  src?: string;
+}
+
+interface IframeElement extends BaseElement {
+  tag: 'iframe';
+  src?: string;
+}
+
+interface ImgElement extends BaseElement {
+  tag: 'img';
+  src?: string;
+}
+
+interface InputElement extends BaseElement {
+  tag: 'input';
+  placeholder?: string;
+  type?: string;
+}
+
+interface LiElement extends BaseElement {
+  tag: 'li';
+}
+
+interface SectionElement extends BaseElement {
+  tag: 'section';
+  src?: string;
+}
+
+interface SpanElement extends BaseElement {
+  tag: 'span';
+}
+
+interface UlElement extends BaseElement {
+  tag: 'ul';
+}
+
+export interface ISpec {
+  name: string;
+  description?: string;
+  license?: License;
+  author?: Author;
+  components?: Record<string, Component>;
+  pages?: Record<string, Page>;
+}
+
+export type ShadcnComponent =
   | AccordionComponent
   | AccordionItemComponent
   | AccordionTriggerComponent
@@ -91,30 +136,30 @@ export type Element =
   | AlertTitleComponent
   | AlertDescriptionComponent
   | AlertDialogComponent
-  | AlertDialogContentComponent
-  | AlertDialogOverlayComponent
-  | AlertDialogPortalComponent
   | AlertDialogTriggerComponent
+  | AlertDialogPortalComponent
+  | AlertDialogOverlayComponent
+  | AlertDialogContentComponent
   | AspectRatioComponent
   | AvatarComponent
-  | AvatarFallbackComponent
   | AvatarImageComponent
+  | AvatarFallbackComponent
   | BadgeComponent
   | BreadcrumbComponent
-  | BreadcrumbEllipsisComponent
+  | BreadcrumbListComponent
   | BreadcrumbItemComponent
   | BreadcrumbLinkComponent
-  | BreadcrumbListComponent
-  | BreadcrumbPageComponent
   | BreadcrumbSeparatorComponent
+  | BreadcrumbPageComponent
+  | BreadcrumbEllipsisComponent
   | ButtonComponent
   | CalendarComponent
   | CardComponent
-  | CardContentComponent
-  | CardDescriptionComponent
-  | CardFooterComponent
   | CardHeaderComponent
   | CardTitleComponent
+  | CardDescriptionComponent
+  | CardContentComponent
+  | CardFooterComponent
   | CarouselComponent
   | CarouselContentComponent
   | CarouselItemComponent
@@ -123,18 +168,18 @@ export type Element =
   | CheckboxComponent
   | CheckboxIndicatorComponent
   | CollapsibleComponent
-  | CollapsibleContentComponent
   | CollapsibleTriggerComponent
+  | CollapsibleContentComponent
   | CommandComponent
+  | CommandInputComponent
+  | CommandListComponent
   | CommandEmptyComponent
   | CommandGroupComponent
-  | CommandInputComponent
   | CommandItemComponent
-  | CommandListComponent
   | CommandSeparatorComponent
   | CommandShortcutComponent
-  | ContextMenuCheckboxItemComponent
   | ContextMenuComponent
+  | ContextMenuCheckboxItemComponent
   | ContextMenuContentComponent
   | ContextMenuItemComponent
   | ContextMenuLabelComponent
@@ -146,111 +191,288 @@ export type Element =
   | ContextMenuSubTriggerComponent
   | ContextMenuTriggerComponent;
 
-export interface AElement extends BaseElement {
-  tag: 'a';
+interface AccordionComponent extends BaseElement {
+  component: 'accordion';
+  collapsible?: boolean;
+  dir?: 'ltr' | 'rtl';
+  disabled?: boolean;
+  orientation?: 'horizontal' | 'vertical';
+  type?: 'single' | 'multiple';
+}
+
+interface AccordionItemComponent extends BaseElement {
+  component: 'accordion-item';
+  disabled?: boolean;
+  value: string;
+}
+
+interface AccordionTriggerComponent extends BaseElement {
+  component: 'accordion-trigger';
+}
+
+interface AccordionContentComponent extends BaseElement {
+  component: 'accordion-content';
+  forceMount?: boolean;
+}
+
+interface AlertComponent extends BaseElement {
+  component: 'alert';
+}
+
+interface AlertTitleComponent extends BaseElement {
+  component: 'alert-title';
+}
+
+interface AlertDescriptionComponent extends BaseElement {
+  component: 'alert-description';
+}
+
+interface AlertDialogComponent extends BaseElement {
+  component: 'alert-dialog';
+  defaultOpen?: boolean;
+  open?: boolean;
+}
+
+interface AlertDialogTriggerComponent extends BaseElement {
+  component: 'alert-dialog-trigger';
+}
+
+interface AlertDialogPortalComponent extends BaseElement {
+  component: 'alert-dialog-portal';
+  forceMount?: boolean;
+  container?: Element;
+}
+
+interface AlertDialogOverlayComponent extends BaseElement {
+  component: 'alert-dialog-overlay';
+  forceMount?: boolean;
+}
+
+interface AlertDialogContentComponent extends BaseElement {
+  component: 'alert-dialog-content';
+  forceMount?: boolean;
+}
+
+interface AspectRatioComponent extends BaseElement {
+  component: 'aspect-ratio';
+  ratio?: number;
+}
+
+interface AvatarComponent extends BaseElement {
+  component: 'avatar';
+}
+
+interface AvatarImageComponent extends BaseElement {
+  component: 'avatar-image';
+}
+
+interface AvatarFallbackComponent extends BaseElement {
+  component: 'avatar-fallback';
+  delayMs?: number;
+}
+
+interface BadgeComponent extends BaseElement {
+  component: 'badge';
+}
+
+interface BreadcrumbComponent extends BaseElement {
+  component: 'breadcrumb';
+}
+
+interface BreadcrumbListComponent extends BaseElement {
+  component: 'breadcrumb-list';
+}
+
+interface BreadcrumbItemComponent extends BaseElement {
+  component: 'breadcrumb-item';
+}
+
+interface BreadcrumbLinkComponent extends BaseElement {
+  component: 'breadcrumb-link';
   href?: string;
-  target?: string;
 }
 
-export interface IframeElement extends BaseElement {
-  tag: 'iframe';
-  src?: string;
+interface BreadcrumbSeparatorComponent extends BaseElement {
+  component: 'breadcrumb-separator';
 }
 
-export interface ImgElement extends BaseElement {
-  tag: 'img';
-  alt?: string;
-  src?: string;
+interface BreadcrumbPageComponent extends BaseElement {
+  component: 'breadcrumb-page';
 }
 
-export interface InputElement extends BaseElement {
-  tag: 'input';
-  placeholder?: string;
-  type?: string;
+interface BreadcrumbEllipsisComponent extends BaseElement {
+  component: 'breadcrumb-ellipsis';
+}
+
+interface ButtonComponent extends BaseElement {
+  component: 'button';
+  asChild?: boolean;
+  disabled?: boolean;
+  variant?: 'link' | 'ghost' | 'outline' | 'secondary';
+  size?: 'icon';
+}
+
+interface CalendarComponent extends BaseElement {
+  component: 'calendar';
+}
+
+interface CardComponent extends BaseElement {
+  component: 'card';
+}
+
+interface CardHeaderComponent extends BaseElement {
+  component: 'card-header';
+}
+
+interface CardTitleComponent extends BaseElement {
+  component: 'card-title';
+}
+
+interface CardDescriptionComponent extends BaseElement {
+  component: 'card-description';
+}
+
+interface CardContentComponent extends BaseElement {
+  component: 'card-content';
+}
+
+interface CardFooterComponent extends BaseElement {
+  component: 'card-footer';
+}
+
+interface CarouselComponent extends BaseElement {
+  component: 'carousel';
+}
+
+interface CarouselContentComponent extends BaseElement {
+  component: 'carousel-content';
+}
+
+interface CarouselItemComponent extends BaseElement {
+  component: 'carousel-item';
+}
+
+interface CarouselNextComponent extends BaseElement {
+  component: 'carousel-next';
+}
+
+interface CarouselPreviousComponent extends BaseElement {
+  component: 'carousel-previous';
+}
+
+interface CheckboxComponent extends Omit<BaseElement, 'defaultChecked'> {
+  component: 'checkbox';
+  defaultChecked?: boolean;
+  checked?: boolean;
+  disabled?: boolean;
+  required?: boolean;
+  name?: string;
   value?: string;
 }
 
-export type ElementArrayOrRef =
-  | Array<Element>
-  | {
-      $ref: Element;
-    };
+interface CheckboxIndicatorComponent extends BaseElement {
+  component: 'checkbox-indicator';
+  forceMount?: boolean;
+}
 
-export interface ISpec {
-  title?: string;
-  name?: string;
-  version?: string;
-  license: 'Apache-2.0' | 'GPL-2.0-only' | 'GPL-3.0-only' | 'ISC' | 'MIT';
-  description?: string;
-  actions?: {
-    [name: string]: {
-      props: {
-        [name: string]: {
-          required?: boolean;
-          type: 'boolean' | 'number' | 'string';
-        };
-      };
-      operations: {
-        type: 'delete' | 'insert' | 'update' | 'revalidate' | 'redirect';
-        model?: string;
-        data?: {
-          [name: string]: any;
-        };
-        path?: string;
-        where?: {
-          [name: string]: any;
-        };
-      }[];
-    };
-  };
-  author?: {
-    name?: string;
-    email?: string;
-    url?: string;
-  };
-  auth?: {
-    providers: Provider[];
-  };
-  calls?: {
-    [name: string]: {
-      request: {
-        [name: string]: {
-          required?: boolean;
-          type: 'boolean' | 'number' | 'string';
-        };
-      };
-      response: {
-        [name: string]: {
-          required?: boolean;
-          type: 'boolean' | 'number' | 'string';
-        };
-      };
-    };
-  };
-  components?: {
-    [name: string]: {
-      props?: Record<
-        string,
-        {
-          required?: boolean;
-          type: string;
-        }
-      >;
-      elements?: ElementArrayOrRef;
-    };
-  };
-  models?: {
-    [name: string]: {
-      attributes: {
-        [name: string]: {
-          key?: 'primary';
-          type: 'boolean' | 'number' | 'string';
-          unique?: boolean;
-        };
-      };
-    };
-  };
-  pages?: {
-    [name: string]: Page;
-  };
+interface CollapsibleComponent extends BaseElement {
+  component: 'collapsible';
+  defaultOpen?: boolean;
+  disabled?: boolean;
+  open?: boolean;
+}
+
+interface CollapsibleTriggerComponent extends BaseElement {
+  component: 'collapsible-trigger';
+}
+
+interface CollapsibleContentComponent extends BaseElement {
+  component: 'collapsible-content';
+  forceMount?: boolean;
+}
+
+interface CommandComponent extends BaseElement {
+  component: 'command';
+}
+
+interface CommandInputComponent extends BaseElement {
+  component: 'command-input';
+  placeholder?: string;
+}
+
+interface CommandListComponent extends BaseElement {
+  component: 'command-list';
+}
+
+interface CommandEmptyComponent extends BaseElement {
+  component: 'command-empty';
+}
+
+interface CommandGroupComponent extends BaseElement {
+  component: 'command-group';
+  heading?: string;
+}
+
+interface CommandItemComponent extends BaseElement {
+  component: 'command-item';
+}
+
+interface CommandSeparatorComponent extends BaseElement {
+  component: 'command-separator';
+}
+
+interface CommandShortcutComponent extends BaseElement {
+  component: 'command-shortcut';
+}
+
+interface ContextMenuComponent extends BaseElement {
+  component: 'context-menu';
+}
+
+interface ContextMenuCheckboxItemComponent extends BaseElement {
+  component: 'context-menu-checkbox-item';
+  checked?: boolean;
+}
+
+interface ContextMenuContentComponent extends BaseElement {
+  component: 'context-menu-content';
+}
+
+interface ContextMenuItemComponent extends BaseElement {
+  component: 'context-menu-item';
+  disabled?: boolean;
+  inset?: boolean;
+}
+
+interface ContextMenuLabelComponent extends BaseElement {
+  component: 'context-menu-label';
+}
+
+interface ContextMenuRadioGroupComponent extends BaseElement {
+  component: 'context-menu-radio-group';
+  value?: string;
+}
+
+interface ContextMenuRadioItemComponent extends BaseElement {
+  component: 'context-menu-radio-item';
+}
+
+interface ContextMenuSeparatorComponent extends BaseElement {
+  component: 'context-menu-separator';
+}
+
+interface ContextMenuSubComponent extends BaseElement {
+  component: 'context-menu-sub';
+}
+
+interface ContextMenuSubContentComponent extends BaseElement {
+  component: 'context-menu-sub-content';
+}
+
+interface ContextMenuSubTriggerComponent extends BaseElement {
+  component: 'context-menu-sub-trigger';
+}
+
+interface ContextMenuTriggerComponent extends BaseElement {
+  component: 'context-menu-trigger';
 }
