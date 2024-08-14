cca2e6a60b2b14cb402efc30dee0e68d6512ce22
Chris Tate <chris@ctate.dev>
2024-08-14 08:55:28 -0500

    updates next example

--- examples/next-api/app/layout.tsx
+++ examples/next-api/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -12,7 +13,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
