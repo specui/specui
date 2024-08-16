216477fe831ca5a92930afe91f73c5db69b4ab32
Chris Tate <chris@ctate.dev>
2024-08-16 01:57:53 -0500

    adds margin/padding tailwind classes

--- generators/next/.specui/types/TailwindClass.ts
+++ generators/next/.specui/types/TailwindClass.ts
@@ -1,3 +1,39 @@
+type Size =
+  | '0'
+  | '0.5'
+  | '1'
+  | '1.5'
+  | '2'
+  | '2.5'
+  | '3'
+  | '3.5'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | '10'
+  | '11'
+  | '12'
+  | '14'
+  | '16'
+  | '20'
+  | '24'
+  | '28'
+  | '32'
+  | '36'
+  | '40'
+  | '44'
+  | '48'
+  | '52'
+  | '56'
+  | '60'
+  | '64'
+  | '72'
+  | '80'
+  | '96';
+
 export type TailwindClass =
   | 'absolute'
   | 'align-baseline'
@@ -1548,4 +1584,18 @@ export type TailwindClass =
   | 'z-30'
   | 'z-40'
   | 'z-50'
-  | 'z-auto';
+  | 'z-auto'
+  | `m-${Size}`
+  | `mb-${Size}`
+  | `ml-${Size}`
+  | `mr-${Size}`
+  | `mt-${Size}`
+  | `mx-${Size}`
+  | `my-${Size}`
+  | `p-${Size}`
+  | `pb-${Size}`
+  | `pl-${Size}`
+  | `pr-${Size}`
+  | `pt-${Size}`
+  | `px-${Size}`
+  | `py-${Size}`;
