41e52fd07655f778bb7687d36e51646c208ed2a2
Chris Tate <chris@ctate.dev>
2024-10-28 22:48:03 -0500

    next file updates

--- generators/next/.specui/generator.ts
+++ generators/next/.specui/generator.ts
@@ -1698,27 +1698,6 @@ fn main() {
         }
       `,
     }),
-    'public/next.svg': await generate({
-      processor: PrettierProcessor({
-        parser: 'html',
-      }),
-      engine: () => `
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 394 80">
-          <path fill="#000" d="M262 0h68.5v12.7h-27.2v66.6h-13.6V12.7H262V0ZM149 0v12.7H94v20.4h44.3v12.6H94v21h55v12.6H80.5V0h68.7zm34.3 0h-17.8l63.8 79.4h17.9l-32-39.7 32-39.6h-17.9l-23 28.6-23-28.6zm18.3 56.7-9-11-27.1 33.7h17.8l18.3-22.7z"/>
-          <path fill="#000" d="M81 79.3 17 0H0v79.3h13.6V17l50.2 62.3H81Zm252.6-.4c-1 0-1.8-.4-2.5-1s-1.1-1.6-1.1-2.6.3-1.8 1-2.5 1.6-1 2.6-1 1.8.3 2.5 1a3.4 3.4 0 0 1 .6 4.3 3.7 3.7 0 0 1-3 1.8zm23.2-33.5h6v23.3c0 2.1-.4 4-1.3 5.5a9.1 9.1 0 0 1-3.8 3.5c-1.6.8-3.5 1.3-5.7 1.3-2 0-3.7-.4-5.3-1s-2.8-1.8-3.7-3.2c-.9-1.3-1.4-3-1.4-5h6c.1.8.3 1.6.7 2.2s1 1.2 1.6 1.5c.7.4 1.5.5 2.4.5 1 0 1.8-.2 2.4-.6a4 4 0 0 0 1.6-1.8c.3-.8.5-1.8.5-3V45.5zm30.9 9.1a4.4 4.4 0 0 0-2-3.3 7.5 7.5 0 0 0-4.3-1.1c-1.3 0-2.4.2-3.3.5-.9.4-1.6 1-2 1.6a3.5 3.5 0 0 0-.3 4c.3.5.7.9 1.3 1.2l1.8 1 2 .5 3.2.8c1.3.3 2.5.7 3.7 1.2a13 13 0 0 1 3.2 1.8 8.1 8.1 0 0 1 3 6.5c0 2-.5 3.7-1.5 5.1a10 10 0 0 1-4.4 3.5c-1.8.8-4.1 1.2-6.8 1.2-2.6 0-4.9-.4-6.8-1.2-2-.8-3.4-2-4.5-3.5a10 10 0 0 1-1.7-5.6h6a5 5 0 0 0 3.5 4.6c1 .4 2.2.6 3.4.6 1.3 0 2.5-.2 3.5-.6 1-.4 1.8-1 2.4-1.7a4 4 0 0 0 .8-2.4c0-.9-.2-1.6-.7-2.2a11 11 0 0 0-2.1-1.4l-3.2-1-3.8-1c-2.8-.7-5-1.7-6.6-3.2a7.2 7.2 0 0 1-2.4-5.7 8 8 0 0 1 1.7-5 10 10 0 0 1 4.3-3.5c2-.8 4-1.2 6.4-1.2 2.3 0 4.4.4 6.2 1.2 1.8.8 3.2 2 4.3 3.4 1 1.4 1.5 3 1.5 5h-5.8z"/>
-        </svg>
-      `,
-    }),
-    'public/vercel.svg': await generate({
-      processor: PrettierProcessor({
-        parser: 'html',
-      }),
-      engine: () => `
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 283 64">
-          <path fill="black" d="M141 16c-11 0-19 7-19 18s9 18 20 18c7 0 13-3 16-7l-7-5c-2 3-6 4-9 4-5 0-9-3-10-7h28v-3c0-11-8-18-19-18zm-9 15c1-4 4-7 9-7s8 3 9 7h-18zm117-15c-11 0-19 7-19 18s9 18 20 18c6 0 12-3 16-7l-8-5c-2 3-5 4-8 4-5 0-9-3-11-7h28l1-3c0-11-8-18-19-18zm-10 15c2-4 5-7 10-7s8 3 9 7h-19zm-39 3c0 6 4 10 10 10 4 0 7-2 9-5l8 5c-3 5-9 8-17 8-11 0-19-7-19-18s8-18 19-18c8 0 14 3 17 8l-8 5c-2-3-5-5-9-5-6 0-10 4-10 10zm83-29v46h-9V5h9zM37 0l37 64H0L37 0zm92 5-27 48L74 5h10l18 30 17-30h10zm59 12v10l-3-1c-6 0-10 4-10 10v15h-9V17h9v9c0-5 6-9 13-9z"/>
-        </svg>
-      `,
-    }),
 
     '.eslintrc.json': await generate({
       engine: JsonEngine,
@@ -1773,18 +1752,14 @@ fn main() {
     '.npmrc': await generate({
       engine: () => `engine-strict=true`,
     }),
-    'next.config.js': await generate({
+    'next.config.ts': await generate({
       processor: PrettierProcessor(),
       engine: () => /*ts*/ `
-        /** @type {import('next').NextConfig} */
-        const nextConfig = {
-          experimental: {
-            useWasmBinary: true,
-          },
-          swcMinify: false
-        }
+        import type { NextConfig } from "next";
 
-        module.exports = nextConfig
+        const nextConfig: NextConfig = {};
+        
+        export default nextConfig;
       `,
     }),
     'package.json': await generate({
@@ -1900,15 +1875,17 @@ fn main() {
           : undefined,
       },
     }),
-    'postcss.config.js': await generate({
+    'postcss.config.mjs': await generate({
       processor: PrettierProcessor(),
       engine: () => /* ts */ `
-        module.exports = {
+        /** @type {import('postcss-load-config').Config} */
+        const config = {
           plugins: {
             tailwindcss: {},
-            autoprefixer: {},
           },
-        }      
+        };
+
+        export default config;
       `,
     }),
     'README.md': await generate({
@@ -1919,31 +1896,36 @@ fn main() {
       spec,
       template: ReadmeTemplate,
     }),
-    'tailwind.config.js': await generate({
+    'tailwind.config.ts': await generate({
       processor: PrettierProcessor(),
       engine: () => /*ts*/ `
-        /** @type {import('tailwindcss').Config} */
-        module.exports = {
+        import type { Config } from "tailwindcss";
+
+        const config: Config = {
           content: [
-            "./app/**/*.{js,ts,jsx,tsx,mdx}",
             "./pages/**/*.{js,ts,jsx,tsx,mdx}",
             "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        
-            // Or if using \`src\` directory:
-            "./src/**/*.{js,ts,jsx,tsx,mdx}",
+            "./app/**/*.{js,ts,jsx,tsx,mdx}",
           ],
           theme: {
-            extend: {},
+            extend: {
+              colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+              },
+            },
           },
           plugins: [],
-        }
+        };
+
+        export default config;
       `,
     }),
     'tsconfig.json': await generate({
       engine: JsonEngine,
       spec: {
         compilerOptions: {
-          target: 'es5',
+          target: 'ES2017',
           lib: ['dom', 'dom.iterable', 'esnext'],
           allowJs: true,
           skipLibCheck: true,
